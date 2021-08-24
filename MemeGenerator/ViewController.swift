654cc7add3eb5f2f8fdfbe8dc4a11c08d94d7e50
Igor Chernyshov <Chernyshov.I.Ser@sberbank.ru>
2021-08-24 20:30:59 +0300

    Completed photos import from library

--- MemeGenerator/ViewController.swift
+++ MemeGenerator/ViewController.swift
@@ -7,13 +7,37 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
 
+	// MARK: - Outlets
+	@IBOutlet var imageView: UIImageView!
+	@IBOutlet var hintLabel: UILabel!
+
+	// MARK: - Lifecycle
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		// Do any additional setup after loading the view.
+		navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(newMemeDidTap))
 	}
 
-
+	// MARK: - Actions
+	@objc private func newMemeDidTap() {
+		let imagePickerController = UIImagePickerController()
+		imagePickerController.sourceType = .photoLibrary
+		imagePickerController.delegate = self
+		present(imagePickerController, animated: UIView.areAnimationsEnabled)
+	}
 }
 
+// MARK: - UIImagePickerControllerDelegate
+extension ViewController: UIImagePickerControllerDelegate {
+
+	func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+		guard let image = info[.originalImage] as? UIImage else { return }
+		dismiss(animated: UIView.areAnimationsEnabled)
+		imageView.image = image
+		hintLabel.isHidden = true
+	}
+}
+
+// MARK: - UINavigationControllerDelegate
+extension ViewController: UINavigationControllerDelegate {}
